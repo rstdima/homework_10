9dec9ea29c3c328098d53aa93506a505bf5cde01
“Dmitriy” <colnce1-1@ya.ru>
2022-08-25 15:49:23 +0300

    output of natural numbers in the range from N to 1

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите значение N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersToN(number);
+
+void PrintNumbersToN(int N)
+{
+   if (N < 1) return;
+   Console.Write(N + " ");
+   PrintNumbersToN(N - 1);
+}
