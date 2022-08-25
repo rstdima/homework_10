d3c8214e24a6f81e419430f7ad144b18b1c3a9d5
“Dmitriy” <colnce1-1@ya.ru>
2022-08-25 15:57:21 +0300

    the sum of natural elements in the range from M to N.

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите значение M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int PrintNumbersToN(int m, int n)
+{
+   if (m == n)
+      return n;
+   return n + PrintNumbersToN(m, n - 1);
+}
+Console.WriteLine($"сумма натуральных чисел равна {PrintNumbersToN(m, n)}");
